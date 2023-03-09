82a985ed4d596bc67aa13d0edf6f60f023fff7d0
Raguram <p.raguram121@gmail.com>
2023-03-09 14:10:02 +0530

    Changed Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import styles from  "./Navbar.module.css";
-import logo from '../Footer/Images/logo.jpg'
+import styles from "./Navbar.module.css";
+import logo from "../Footer/Images/logo.jpg";
 const Navbar = () => {
-  const inlinestyles=({isActive})=>
-  {
-    return({
-      fontSize:isActive?"35px":"25px",
-      color:isActive?"red":"white",
-      
-    })
-  }
+  const inlinestyles = ({ isActive }) => {
+    return {
+      fontSize: isActive ? "35px" : "25px",
+      color: isActive ? "red" : "white",
+    };
+  };
   return (
     <div>
-      <nav className={styles.nav} >
+      <nav className={styles.nav}>
         <div className={styles.navheader}>
-          <img  src={logo} alt="dsf"/>
-            <div className={styles.nava}>
-              <NavLink style={inlinestyles} to="/">Home</NavLink>
-              <NavLink style={inlinestyles} to="/about">About</NavLink>
-              <NavLink style={inlinestyles} to="/service">Service</NavLink>
-              <NavLink style={inlinestyles} to="/login">Login</NavLink>
-            </div>
+          <img src={logo} alt="dsf" />
+          <div className={styles.link}>
+            <NavLink style={inlinestyles} to="/">
+              Home
+            </NavLink>
+            <NavLink style={inlinestyles} to="/about">
+              About
+            </NavLink>
+            <NavLink style={inlinestyles} to="/service">
+              Service
+            </NavLink>
+            <NavLink style={inlinestyles} to="/login">
+              Login
+            </NavLink>
+          </div>
         </div>
       </nav>
     </div>
