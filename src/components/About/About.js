ba16074f520c4618a466282b69ed245783464bc2
Raguram <p.raguram121@gmail.com>
2023-03-09 22:47:47 +0530

    About Edited

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,9 @@
 import React from "react";
+// import Img1 from "../../assets/About/about.jpg";
 
 const About = () => {
-  return <div>About</div>;
+  return <div>About </div>;
+
 };
 
 export default About;
