4e93e530ffc3e2d27ae3d36bc5d948cf01d97efd
prakatesh <prakatesh2020@gmail.com>
2023-04-25 15:13:18 +0530

    login added and connected to database

---
+++ server/controller/logincontroller.js
@@ -0,0 +1,29 @@
+const signupModules=require('../modules/signup')
+
+exports.login=async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        const data=await signupModules.findOne({email:email})
+
+            if(!data)
+            {
+                res.json("wrong_user")
+                console.log("wrong user")
+            }
+
+            else if(data.password!==password)
+            {
+                res.json("wrong_password")
+                console.log("wrong password")
+            }
+            else
+            {
+                res.json("correct_user")
+                console.log("correct user")
+            }
+    }
+    catch(e)
+    {
+        console.log(e)
+    }
+}
